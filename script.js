2195925eafc088e3ca7538f6a9b8b631b1a60043
markeezy <markkhoo95@gmail.com>
2021-04-02 01:05:27 -0700

    add load for stored object

--- script.js
+++ script.js
@@ -22,3 +22,34 @@ timeblock($("#02pm"),14);
 timeblock($("#03pm"),15);
 timeblock($("#04pm"),16);
 timeblock($("#05pm"),17);
+
+// Create Empty Object if none stored
+var scheduler = {
+    am09: "",
+    am10: "",
+    am11: "",
+    pm12: "",
+    pm01: "",
+    pm02: "",
+    pm03: "",
+    pm04: "",
+    pm05: ""
+};
+
+// Load stored schedule
+var storedSchedule = JSON.parse(localStorage.getItem("storedSchedule"));
+if (storedSchedule !== null) {
+    $("#text09").text(storedSchedule.am09);
+    $("#text10").text(storedSchedule.am10);
+    $("#text11").text(storedSchedule.am11);
+    $("#text12").text(storedSchedule.pm12);
+    $("#text13").text(storedSchedule.pm01);
+    $("#text14").text(storedSchedule.pm02);
+    $("#text15").text(storedSchedule.pm03);
+    $("#text16").text(storedSchedule.pm04);
+    $("#text17").text(storedSchedule.pm05);
+} else {
+    // Store Object if none exists
+    localStorage.setItem("storedSchedule", JSON.stringify(scheduler));
+};
+
